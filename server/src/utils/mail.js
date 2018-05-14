43832e327c891f2578d17ef9c2e79fd37dee15db
alanpramuk9 <alanpramuk9@gmail.com>
2018-05-14 12:42:32 -0500

    more chnages

--- server/src/utils/mail.js
+++ server/src/utils/mail.js
@@ -1,10 +1,9 @@
 import mailgunloader from 'mailgun-js';
-import cfg from '../config/env/development';
+import config from '../config';
 //let api_key = 'cfg.mailgun_api_key';
-let api_key = 'key-5307bc90cff63a3a5707307a7984ea19';
 let domain = 'sandboxe08fbb44d35e48d9aa500535b31d9d39.mailgun.org';
 
-let mailgun = mailgunloader({apiKey: api_key, domain: domain });
+let mailgun = mailgunloader({apiKey: config.MAILGUN_API_KEY, domain: domain });
 
 function sendEmail(to, from, subject, content) {
     let data ={
